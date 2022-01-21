40fa145658234115dccf8e1c040f0c8478e99fd0
brenorodriguesdev <brenorodriguesdev@gmail.com>
2022-01-21 00:23:47 -0300

    feat: setup database with env vars

--- src/main/config/database.ts
+++ src/main/config/database.ts
@@ -1,4 +1,13 @@
 
 import { createConnection } from 'typeorm'
 
-createConnection()
+createConnection({
+    type: 'mssql',
+    host: process.env.TYPEORM_HOST,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    entities: [
+        process.env.TYPEORM_ENTITIES
+    ] 
+})
