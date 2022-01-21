4907fb90c20bb84762eb66da5f3c2b102cde0f82
brenorodriguesdev <brenorodriguesdev@gmail.com>
2022-01-21 00:32:57 -0300

    fix: server listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
 import app from './config/app'
 
-app.listen(process.env.PORT || 4500)
 dotenv.config()
+app.listen(process.env.PORT || 4500)
 import './config/database'
